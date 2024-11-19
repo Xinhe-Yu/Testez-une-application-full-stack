7e76564bf58973842ba68c43668a63bacf9beab7
Xinhe YU <aspasia.yu@gmail.com>
2024-11-19 22:56:41 +0100

    test 3 entity services

--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -1,22 +1,52 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { TeacherService } from './teacher.service';
+import { Teacher } from '../interfaces/teacher.interface';
 
 describe('TeacherService', () => {
   let service: TeacherService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [TeacherService]
     });
     service = TestBed.inject(TeacherService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('detail', () => {
+    it('should return a teacher by id', () => {
+      const mockTeacher: Teacher = {
+        id: 1,
+        lastName: 'Castorp',
+        firstName: 'Hans',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      };
+
+      const teacherId = '1';
+
+      service.detail(teacherId).subscribe(teacher => {
+        expect(teacher).toEqual(mockTeacher);
+      });
+
+      const req = httpMock.expectOne(`${service['pathService']}/${teacherId}`);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockTeacher);
+    });
   });
 });
